a802d98fdde59481ca9ee1e0e9c2190cee1ee3d3
LotusBro98 <LotusBro98@yandex.ru>
2021-09-16 19:57:07 +0300

    Update distribution.py

--- distribution.py
+++ distribution.py
@@ -3,38 +3,7 @@ import matplotlib.pyplot as plt
 
 CHUNK_N = 16
 
-def flatten(x: np.ndarray):
-    if len(x.shape) <= 2:
-        return x
-    return x.reshape((-1, x.shape[-1]))
-
-
-def unpackbits(x, n=CHUNK_N, last_only=False):
-    x0 = np.uint8(x)
-    x1 = np.uint8(x >> 8)
-
-    x0 = np.unpackbits(x0).reshape(x0.shape[:-1] + (-1, 8))
-    x1 = np.unpackbits(x1).reshape(x1.shape[:-1] + (-1, 8))
-    x = np.concatenate([x1, x0], axis=-1)
-
-    if not last_only:
-        x = x.reshape(x.shape[:-2] + (-1,))
-    x = x[..., -n:]
-
-    return x
-
-
-def packbits(x, n=CHUNK_N, last_only=False):
-    rest = (x.shape[-1] // CHUNK_N + (x.shape[-1] % CHUNK_N != 0)) * CHUNK_N - x.shape[-1]
-    x = np.pad(x, [(0, 0)] * (len(x.shape) - 1) + [(rest, 0)], constant_values=0)
-    if not last_only:
-        x = x.reshape(x.shape[:-1] + (-1, CHUNK_N))
-
-    x1, x0 = np.split(x, 2, axis=-1)
-    x0 = np.packbits(x0, axis=-1).squeeze(-1)
-    x1 = np.packbits(x1, axis=-1).squeeze(-1)
-    x = (np.uint16(x1) << 8) + np.uint16(x0)
-    return x
+from operations import unpackbits
 
 
 def get_cross_probability_table(x):
@@ -49,6 +18,7 @@ def get_cross_probability_table(x):
 
     print(px)
     return a
+
 
 def test_data(x):
     A = get_cross_probability_table(x)
